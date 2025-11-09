e5c1e2cb04e4d3ad8de3c6ebece746c8100a27d3
mirzashary <86553601+mirzashary@users.noreply.github.com>
2025-11-09 18:07:39 +0530

    Create Script_23.sh
    testing merge

---
+++ Script_23.sh
@@ -0,0 +1,5 @@
+1.echo "We are trying this practical from linux EC2"
+2.echo "We are from batch-25 and learning git/gihub"
+3.echo "We are learning Branching"
+4.echo "I am learning Devops"
+5.echo "Adding new line"
